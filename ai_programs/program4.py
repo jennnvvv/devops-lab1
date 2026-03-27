18b95f201fa6b141a802d130eea522c23dd508bf
irotheree-hkk <irotheree@gmail.com>
2026-03-27 15:48:53 +0530

    program4 commit

---
+++ ai_programs/program4.py
@@ -0,0 +1,16 @@
+# Rational Agent to move towards goal
+
+class RationalAgent:
+    def __init__(self, goal):
+        self.position = 0
+        self.goal = goal
+
+    def move_towards_goal(self):
+        while self.position < self.goal:
+            self.position += 1
+            print(f"Current Position: {self.position}")
+        print("Reached the goal!")
+
+goal_position = 5
+agent = RationalAgent(goal_position)
+agent.move_towards_goal()

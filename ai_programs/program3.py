9d85ef4fe954b627dae4787c29f54ab1a55c4e66
irotheree-hkk <irotheree@gmail.com>
2026-03-27 15:47:17 +0530

    program3 commit

---
+++ ai_programs/program3.py
@@ -0,0 +1,12 @@
+# Move towards goal (Simple Agent)
+
+def move_towards_goal(position, goal):
+    while position < goal:
+        position += 1
+        print(f"Current Position: {position}")
+    print("Reached the goal!")
+
+initial_position = 0
+goal_position = 5
+
+move_towards_goal(initial_position, goal_position)

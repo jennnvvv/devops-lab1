ba8ea60c238ebe6d6a1ea6d8b90b2c325b91de48
jennnvvv <2023cs_jennessajoellavalder_a@nie.ac.in>
2026-04-10 10:39:40 +0000

    feat: add dockerized flask app with nginx and redis

--- devops-lab/app/app.py
+++ devops-lab/app/app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
+import redis, os
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello from DevOps Lab"
+r = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=6379,
+    decode_responses=True
+)
 
-app.run(host="0.0.0.0", port=5000)
+@app.route("/")
+def index():
+    count = r.incr("visits")
+    return jsonify({
+        "message": "Hello from the DevOps Lab!",
+        "hostname": os.uname().nodename,
+        "visits": count
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
